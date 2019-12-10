771f97b03c2cb6f0f37b7f9d2cef6e98072a3d55
huoqikun <h15010653940@163.com>
2019-12-10 11:49:54 +0800

    03

--- api/api_employee.py
+++ api/api_employee.py
@@ -12,7 +12,6 @@ class ApiEmployee:
         # {}为占位符,传入员工id
         self.url_delete = api.BASE_URL + "/api/sys/user/{}"
 
-
     #  添加员工
     def api_post_employee(self,name,pwd,num):
         # 创建json数据
