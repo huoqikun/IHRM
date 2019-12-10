87d4d37c3ef3ca8a01c6871ab69cddcb15d990ce
huoqikun <h15010653940@163.com>
2019-12-10 17:24:53 +0800

    07

--- run_suit.py
+++ run_suit.py
@@ -1,7 +1,8 @@
 # 导包
 import unittest
-from tools.HTMLTestRunner import HTMLTestRunner
 # 定义测试套件
+from tools.HTMLTestReportCN import HTMLTestRunner
+
 suite=unittest.defaultTestLoader.discover("./scripts")
 # 获取报告存储位置,实例化HTMLTestRunner     调用run
 with open("./report/report.html","wb") as f:
